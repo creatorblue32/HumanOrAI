aab6107bad5f6e775e18544ab21cf088839b2455
elyasmasrour <elyasmasrour@gmail.com>
2024-04-05 14:34:43 -0400

    AI Call Trial

--- api/ai_call.py
+++ api/ai_call.py
@@ -60,7 +60,7 @@ class handler(BaseHTTPRequestHandler):
         prompt += "In the spring of 2023, Sunshine Henle found herself grappling with the profound loss of her 72-year-old mother, who succumbed to organ failure the previous Thanksgiving. Amidst her grief, Henle turned to an unconventional source of comfort: artificial intelligence. Leveraging OpenAI's ChatGPT, she crafted a \"ghostbot\" of her mother, infusing it with their shared text messages to simulate conversations that echoed her mother's voice and wisdom. This innovative approach to coping with her loss proved to be a source of solace for Henle, a Florida-based AI trainer accustomed to the potential of technology to mimic human interactions. Henle's experience is situated within the burgeoning landscape of \"grief tech,\" a niche but rapidly expanding field that intersects technology and bereavement support. Startups like Replika, HereAfter AI, StoryFile, and Seance AI are at the forefront of this movement, offering a variety of services designed to help individuals navigate their grief. These platforms employ deep learning and large language models to recreate the essence of lost loved ones, providing interactive video conversations, virtual avatars for texting, and audio legacies that aim to preserve the memory and presence of the deceased. Despite the comfort these technologies offer to those like Henle, they also usher in a host of ethical and psychological dilemmas. Questions about the consent of the deceased, the potential for psychological dependency on digital avatars, and the risks of exacerbating grief through artificial prolongation of relationships are at the heart of the debate. Furthermore, the commercialization of grief, with services ranging from affordable subscriptions to premium packages, raises concerns about the exploitation of vulnerable individuals seeking closure."
         prompt += "Comment Section: Comment 1:"
 
-        if model == "GPT-2":
+        if model == "gpt2":
             API_URL = "https://api-inference.huggingface.co/models/openai-community/gpt2"
             hfapikey = os.getenv('HF_API_KEY')
             headers = {"Authorization": "Bearer "+hfapikey}
@@ -72,17 +72,17 @@ class handler(BaseHTTPRequestHandler):
             }
             generated_text = requests.post(API_URL, headers=headers, json=payload).json()
 
-        if model == "LLAMA":
+        elif model == "LLAMA":
             generated_text = "LLAMA still unimplemented"
             
-        if model == "GPT-3.5":
+        elif model == "gpt3.5":
             openai.api_key = os.getenv('OPENAI_API_KEY')
             response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                                 messages=[{"role": "system", "content": ""},
                                                     {"role": "user", "content": prompt}])
             generated_text = response["choices"][0]["message"]["content"]
         else:
-            generated_text = "Unrecognized ModelL "
+            generated_text = "Unrecognized Model: "
             generated_text += model
                         
         
