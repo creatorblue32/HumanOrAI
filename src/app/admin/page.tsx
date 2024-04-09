3a71057977a21a7c7b6d6de1ce8fcb02a3c659d4
elyasmasrour <elyasmasrour@gmail.com>
2024-04-09 08:45:30 -0400

    Added logging to verify function call

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -12,6 +12,7 @@ export default function Page() {
   const [gameId, setGameId] = useState<string | null>(null);
 
   const updateGameId = (newGameId: string) => {
+    console.log("YESS IT'S HAPPENING!")
     setGameId(newGameId);
   };
 
