f183a23599b128244210fee83fe4ed23e7bee30a
elyasmasrour <elyasmasrour@gmail.com>
2024-04-09 11:48:29 -0400

    Updated Answers Page

--- api/begin_game.py
+++ api/begin_game.py
@@ -87,7 +87,7 @@ class handler(BaseHTTPRequestHandler):
             blank_user = {"state": "waiting", "comment": "", "vote":""}
 
             #Handle Model Assignment
-            models = ["GPT-2","GPT-3.5","LLAMA","LLAMA-2","Gemini"]
+            models = ["LLAMA2", "GPT-2","GPT-3.5","LLAMA","Gemini"]
             modelIndex = 0
 
 
